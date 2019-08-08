80267b86b8a386fe41810d62be0bcbc365caf4a2
Boris Savic <bsavic@codebluestudio.com>
2019-08-08 20:35:52 +0200

    Updates

--- views/frontend/default/_brief.php
+++ views/frontend/default/_brief.php
@@ -1,38 +1,21 @@
 <?php
-/**
- * Project: yii2-blog for internal using
- * Author: akiraz2
- * Copyright (c) 2018.
- */
-
 use yii\helpers\Html;
 
+$banner = $model->getImageFileUrl('banner');
 ?>
 
-<div class="blog-brief">
-    <div class="blog-brief__img">
-        <?= Html::img($model->getImageFileUrl('banner')); ?>
-    </div>
-    <div class="blog-brief__wrap">
-        <div class="blog-brief__cat">
-            <?= $model->category->title; ?>
-        </div>
-        <h4 class="blog-brief__title title title--4">
-            <?= Html::a(Html::encode($model->title), $model->url); ?>
-        </h4>
-        <div class="blog-brief__content">
-            <?php
-            echo \yii\helpers\HtmlPurifier::process($model->brief);
-            ?>
-        </div>
-        
-        <div class="blog-brief__nav">
-            <span>
-                <i class="fa fa-calendar"></i><?= Yii::$app->formatter->asDate($model->created_at); ?>
-            </span>
-            <span>
-                <i class="fa fa-eye"></i><?= $model->click; ?>
-            </span>
+<div class="col-12 col-md-6 col-lg-4">
+    <div class="card mt-4">
+        <?php if (!is_null($banner)) { ?>
+        <a href="<?= $model->url ?>">
+            <img class="card-img-top" alt="<?= Html::encode($model->title) ?>" src="<?= $banner ?>" />
+        </a>
+        <?php } ?>
+
+        <div class="card-body">
+            <span class="font-size-md tc-secundary"><?= Yii::$app->formatter->asDate($model->created_at) ?> by <?= $model->user->username ?></span>
+            <h2 class="pt-3 font-size-xlg tc-primary font-light"><?= Html::a(Html::encode($model->title), $model->url); ?></h2>
+            <p class="pt-3 font-size-xmd tc-primary"><?= \yii\helpers\HtmlPurifier::process($model->brief) ?></p>
         </div>
     </div>
 </div>
--- views/frontend/default/index.php
+++ views/frontend/default/index.php
@@ -8,72 +8,64 @@
 use akiraz2\blog\Module;
 use yii\widgets\ListView;
 
-\akiraz2\blog\assets\AppAsset::register($this);
+$this->title = 'Blog';
 
-$this->title = Module::t('blog', 'Blog');
 Yii::$app->view->registerMetaTag([
     'name' => 'description',
-    'content' => Yii::$app->name . ' ' . Module::t('blog', 'Blog')
+    'content' => 'Blog about Design, Ionic 4, React, Flutter & Android frameworks'
 ]);
 Yii::$app->view->registerMetaTag([
     'name' => 'keywords',
-    'content' => Yii::$app->name . ', ' . Module::t('blog', 'Blog')
+    'content' => 'Design, Ionic 4, React, Flutter, Android'
 ]);
 
 if (Yii::$app->get('opengraph', false)) {
     Yii::$app->opengraph->set([
         'title' => $this->title,
         'description' => Module::t('blog', 'Blog'),
-        //'image' => '',
     ]);
 }
-//$this->params['breadcrumbs'][] = '文章';
-
-/*$this->breadcrumbs=[
-    //$post->category->title => Yii::app()->createUrl('post/category', array('id'=>$post->category->id, 'slug'=>$post->category->slug)),
-    '文章',
-];*/
-
 ?>
-<div class="blog-index">
-    <div class="blog-index__header">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-7">
-                    <h1 class="title title--1"><?= Module::t('blog', 'Blog'); ?></h1>
-                </div>
-                <div class="col-md-5">
-                    <div class="blog-index__search">
-                        <?= \yii\widgets\Menu::widget([
-                            'items' => $cat_items,
-                            'options' => [
-                                'class' => 'blog-index__cat'
-                            ]
-                        ]);
-                        ?>
-                    </div>
-                </div>
-            </div>
-        
-        </div>
-    </div>
-    
+
+
+<section class="my-5 about-page">
     <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <?php
-                echo ListView::widget([
-                    'dataProvider' => $dataProvider,
-                    'itemView' => '_brief',
-                    'options' => [
-                        'class' => 'blog-list-view'
-                    ],
-                    'layout' => '{items}{pager}{summary}'
-                ]);
-                ?>
+        <div class="row mb-5">
+            <div class="col-12">
+                <h1 class="font-size-xxlg tc-primary ml-5 mt-5 font-bold">DevsPush Blog</h1>
+                <p class="font-size-lg tc-primary ml-5">Blog about Design, Ionic 4, React, Flutter & Android frameworks</p>
             </div>
         </div>
+
+        <?= ListView::widget([
+            'dataProvider' => $dataProvider,
+            'itemView' => '_brief',
+            'options' => [
+                'class' => 'row',
+                'id' => false
+            ],
+            'itemOptions' => [
+                'tag' => false
+            ],
+            'layout' => '{items}{pager}'
+        ]); ?>
+
+        <?php /*
+        <div class="row">
+            <div class="col-12 my-5">
+                <!-- Pagination -->
+                <ul class="pagination d-flex justify-content-center my-3">
+                    <li class="active"><a href="#" class="previous tc-secundary font-size-xmd font-medium">Previous</a></li>
+                    <li class="font-size-xmd font-medium active"><a href="#">1</a></li>
+                    <li class="font-size-xmd font-medium"><a href="#">2</a></li>
+                    <li class="font-size-xmd font-medium"><a href="#">3</a></li>
+                    <li class="font-size-xmd font-medium"><a href="#">4</a></li>
+                    <li><a href="#" class="next tc-secundary font-size-xmd font-medium">Next</a></li>
+                </ul>
+            </div>
+        </div>
+        */ ?>
     </div>
-</div>
+</section>
 
 
