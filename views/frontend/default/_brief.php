d82723f38dc52d3e40fba17527ee489ae8700e16
Boris Savic <bsavic@codebluestudio.com>
2019-10-23 20:19:14 +0200

    URL update

--- views/frontend/default/_brief.php
+++ views/frontend/default/_brief.php
@@ -17,7 +17,7 @@ $banner = $model->getThumbFileUrl('banner');
 
         <div class="card-body">
             <span class="font-size-md tc-secundary"><?= Yii::$app->formatter->asDate($model->created_at) ?> by <?= $model->user->username ?></span>
-            <h2 class="pt-3 font-size-xlg tc-primary font-light"><?= Html::a(Html::encode($model->title), $model->url); ?></h2>
+            <h2><?= Html::a(Html::encode($model->title), $model->url, ['class' => 'pt-3 font-size-xlg tc-primary font-light']); ?></h2>
             <p class="pt-3 font-size-xmd tc-primary"><?= \yii\helpers\HtmlPurifier::process($model->brief) ?></p>
         </div>
     </div>
